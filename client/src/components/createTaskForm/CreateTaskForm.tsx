9bd7666203479a482a5537381f494922f4364763
Krunal Raol <krnlraol@gmail.com>
2023-04-10 10:01:31 -0400

    Completed the form component

--- client/src/components/createTaskForm/CreateTaskForm.tsx
+++ client/src/components/createTaskForm/CreateTaskForm.tsx
@@ -2,6 +2,10 @@ import React, { FC, ReactElement } from 'react';
 import { Box, Typography, Stack } from '@mui/material';
 import { TaskTitleField } from './_taskTitleField';
 import { TaskDescriptionField } from './_taskDescriptionField';
+import { TaskDateField } from './_taskDateField';
+import { TaskSelectField } from './_taskSelectField';
+import { Status } from './enums/Status';
+import { Priority } from './enums/Priority';
 
 export const CreateTaskForm: FC = (): ReactElement => {
   return (
@@ -19,6 +23,29 @@ export const CreateTaskForm: FC = (): ReactElement => {
       <Stack sx={{ width: '100%' }} spacing={2}>
         <TaskTitleField />
         <TaskDescriptionField />
+        <TaskDateField />
+        <Stack sx={{ width: '100%' }} direction="row" spacing={2}>
+          <TaskSelectField
+            label="Status"
+            name="status"
+            items={[
+              { value: Status.todo, label: Status.todo.toUpperCase() },
+              {
+                value: Status.inProgress,
+                label: Status.inProgress.toUpperCase(),
+              },
+            ]}
+          />
+          <TaskSelectField
+            label="Proprity"
+            name="priority"
+            items={[
+              { value: Priority.high, label: Priority.high.toUpperCase() },
+              { value: Priority.normal, label: Priority.normal.toUpperCase() },
+              { value: Priority.low, label: Priority.low.toUpperCase() },
+            ]}
+          />
+        </Stack>
       </Stack>
     </Box>
   );
--- client/src/components/createTaskForm/_taskDescriptionField.tsx
+++ client/src/components/createTaskForm/_taskDescriptionField.tsx
@@ -1,6 +1,7 @@
 import React, { FC, ReactElement } from 'react';
 import { TextField } from '@mui/material';
 import { ITextField } from './interfaces/ITextField';
+import PropTypes from 'prop-types';
 
 export const TaskDescriptionField: FC<ITextField> = (props): ReactElement => {
   const { onChange = (e) => console.log(e), disabled = false } = props;
@@ -20,4 +21,9 @@ export const TaskDescriptionField: FC<ITextField> = (props): ReactElement => {
       onChange={onChange}
     />
   );
+};
+
+TaskDescriptionField.propTypes = {
+  onChange: PropTypes.func,
+  disabled: PropTypes.bool,
 };
