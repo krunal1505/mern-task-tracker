a0cd9c3fcff5dc7061731bf68961b7fe6fe62e15
Krunal Raol <krnlraol@gmail.com>
2023-04-12 11:04:37 -0400

    Completed front-end part of project

--- client/src/components/task/_taskHeader.tsx
+++ client/src/components/task/_taskHeader.tsx
@@ -8,7 +8,7 @@ export const TaskHeader: FC<ITaskHeader> = (props): ReactElement => {
   const { title = 'Default title', date = new Date() } = props;
 
   return (
-    <Box display="flex" width="100%" justifyContent="space-between" mb={4}>
+    <Box display="flex" width="100%" justifyContent="space-between" mb={3}>
       <Box>
         <Typography variant="h6">{title}</Typography>
       </Box>
