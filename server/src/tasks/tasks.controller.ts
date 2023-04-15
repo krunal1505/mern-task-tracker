d57d6d9010d2319ceb217ef63f57c397c264f678
Krunal Raol <krnlraol@gmail.com>
2023-04-15 13:31:51 -0400

    Added update task route in server

--- server/src/tasks/tasks.controller.ts
+++ server/src/tasks/tasks.controller.ts
@@ -1,8 +1,9 @@
 import { AppDataSource } from '../../index';
 import { Task } from './tasks.entity';
-import { instanceToPlain } from 'class-transformer';
+import { instanceToPlain, plainToInstance } from 'class-transformer';
 import { Request, Response } from 'express';
 import { validationResult } from 'express-validator';
+import { UpdateResult } from 'typeorm';
 
 class TasksController {
   public async getAll(req: Request, res: Response): Promise<Response> {
@@ -18,8 +19,8 @@ class TasksController {
   }
 
   public async create(req: Request, res: Response): Promise<Response> {
-    const errors = validationResult(req);
     try {
+      const errors = validationResult(req);
       if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
       }
@@ -36,6 +37,38 @@ class TasksController {
       );
       createdTask = instanceToPlain(createdTask) as Task;
       return res.status(201).json(createdTask);
+    } catch (_error) {
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
+  }
+
+  public async update(req: Request, res: Response): Promise<Response> {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+
+      const task: Task | null = await AppDataSource.getRepository(Task).findOne(
+        {
+          where: { id: req.body.id },
+        },
+      );
+
+      if (!task) {
+        return res.status(404).json({ error: 'Task not found' });
+      }
+
+      let updatedTask: UpdateResult = await AppDataSource.getRepository(
+        Task,
+      ).update(
+        req.body.id,
+        plainToInstance(Task, {
+          status: req.body.status,
+        }),
+      );
+      updatedTask = instanceToPlain(updatedTask) as UpdateResult;
+      return res.status(200).json(updatedTask);
     } catch (_error) {
       return res.status(500).json({ error: 'Internal Server Error' });
     }
--- server/src/tasks/tasks.router.ts
+++ server/src/tasks/tasks.router.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import { taskController } from './tasks.controller';
-import { createValidator } from './tasks.validator';
+import { createValidator, updateValidator } from './tasks.validator';
 
 export const tasksRouter: Router = Router();
 
 tasksRouter.get('/', taskController.getAll);
 
 tasksRouter.post('/', createValidator, taskController.create);
+
+tasksRouter.put('/', updateValidator, taskController.update);
